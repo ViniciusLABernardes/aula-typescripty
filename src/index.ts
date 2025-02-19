d59398d51b7e677227a48684fe5c94060fc92622
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2025-02-19 20:50:53 -0300

    exercicio 1 e 2 concluidos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 let mensagem :string = "Olá typescript!"; 
 console.log(mensagem);
 
+let valores : number[] = [10,20,30];
+let somaValores : number = valores.reduce((acc, val) => acc + val, 0);
+console.log(somaValores);
+
+function mensagemPerso(mensagem : string):string{
+    return `${mensagem} <<< esta foi sua mensagem!`;
+}
+console.log(mensagemPerso("Meu nome é michael jackson"));
+
