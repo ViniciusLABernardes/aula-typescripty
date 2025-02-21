8d3ff087cdb4b52fd5df8f070c51740bf68f054c
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2025-02-20 22:14:12 -0300

    exercicio 2 - Interfaces e tipagem avançada concluido

--- src/exerciciostype.ts
+++ src/exerciciostype.ts
@@ -11,3 +11,11 @@ const meucarro: Carroo = {
   
 }
 console.log(meucarro);
+
+interface Multiplicacao {
+   (num:number,num2:number): number;
+}
+const multiplicar: Multiplicacao = (n,n2) => n * n2;
+console.log(multiplicar(56,230));
+console.log(multiplicar(12,23));
+console.log(multiplicar(9,520));
