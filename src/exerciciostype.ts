cac26f60b5744d907883ee44af59a6371b81fd1c
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2025-02-20 22:09:41 -0300

    exercicio 1 - Interfaces e tipagem avançada concluido

---
+++ src/exerciciostype.ts
@@ -0,0 +1,13 @@
+interface Carroo{
+    marca:string;
+    modelo:string;
+    ano:number;
+    motor?:string;
+}
+const meucarro: Carroo = {
+    marca: "Hb20",
+    modelo: "Hyundai",
+    ano: 2023,
+  
+}
+console.log(meucarro);
