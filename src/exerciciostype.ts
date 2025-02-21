277acec5c68f013e683b2c94f23d0ff0ab77426c
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2025-02-20 22:26:00 -0300

    exercicio 3 - Generics  concluido

--- src/exerciciostype.ts
+++ src/exerciciostype.ts
@@ -12,6 +12,7 @@ const meucarro: Carroo = {
 }
 console.log(meucarro);
 
+// exercicio 2
 interface Multiplicacao {
    (num:number,num2:number): number;
 }
@@ -19,3 +20,10 @@ const multiplicar: Multiplicacao = (n,n2) => n * n2;
 console.log(multiplicar(56,230));
 console.log(multiplicar(12,23));
 console.log(multiplicar(9,520));
+
+//exercicio 3
+function inverterArray<T>(array: T[]): T[]{
+    return array.reverse();
+}
+console.log(inverterArray([1,2,3,4]));
+console.log(inverterArray(["oi","tudo","bem"]));
