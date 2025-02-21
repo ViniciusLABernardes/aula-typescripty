2db81c5a50db25bc394d95f8fc1b9c394f51ded5
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2025-02-20 22:42:45 -0300

    exercicio 4 - Generics concluido

--- src/exerciciostype.ts
+++ src/exerciciostype.ts
@@ -27,3 +27,32 @@ function inverterArray<T>(array: T[]): T[]{
 }
 console.log(inverterArray([1,2,3,4]));
 console.log(inverterArray(["oi","tudo","bem"]));
+
+//exercicio 4
+interface Repositorio<T>{
+    salvar(dado: T):void ;
+    obterTodos(): T[];
+}
+type Pessoa = {
+    nome : string;
+    email : string;
+}
+
+class RepositorioPessoa implements Repositorio<Pessoa>{
+    private pessoa: Pessoa[] = [];
+
+    salvar(pessoa:Pessoa):void{
+        this.pessoa.push(pessoa);
+    }
+    obterTodos(): Pessoa[] {
+        return this.pessoa;
+    }
+}
+const repositorio = new RepositorioPessoa();
+
+const Maria: Pessoa = { nome: "Maria", email: "maria@email.com" };
+const Marcos: Pessoa = { nome: "Marcos", email: "marcos@email.com" };
+
+repositorio.salvar(Marcos);
+repositorio.salvar(Maria);
+console.log(repositorio.obterTodos());
